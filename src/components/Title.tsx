79a1851a2291c7abe34d4ad54f671b7b458c66a5
kotta-27 <al20013@shibaura-it.ac.jp>
2025-01-22 04:01:56 +0900

    [fix]テキストのカラー修正

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import "../styles/title-style.css";
 
 const Title = () => {
   return (
-    <div>
+    <div className="title-container">
       <div className="title">Img2LaTeX Converter</div>
       <p>画像から数式を認識してTexのコードへ変換します。</p>
     </div>
